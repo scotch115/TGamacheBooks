ee45cb326ee06fba9a83bdac27f39ce8402547b4
scotch115 <jordangamache115@gmail.com>
2019-10-30 10:54:33 -0400

    Wow what a start! Set up main page and skeleton of website

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# TGamacheBooks
-Orlando Indie Author and Coffee Addict
+# T Gamache Books
+> Orlando Indie Author and Coffee Addict
+
+## Welcome to the T Gamache's Website Repository!
+
+Let's be honest: you're probably here by mistake. No worries! The more, the merrier!
+
+Just go ahead and browse around if you'd like, and then when you're ready, there's always the link for ya right here!
+
+//Link back to website here!
---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const express = require('express');
+const router = express.Router();
+const app = express();
+const port = process.argv[2] || 8080;
+
+const mimeType = {
+
+
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css'
+};
+
+const server = http.createServer(function(req, res) 
+{
+  var path = url.parse(req.url).pathname;
+  switch (path) {
+    case '/':
+      res.statusCode = 200;
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      fs.readFile('./public/index.html', null, function(error,data){
+        if (error){
+          res.writeHead(404);
+          res.write("Uh oh! That webpage doesn't exist! :/ \n");
+        } else {
+          res.write(data);
+        }
+        res.end();
+     });
+     break;
+		 
+  default:
+    res.writeHead(404);
+    res.write("Uh oh, this doesn't look right...");
+    res.end();
+    break;
+    }
+ }).listen(port);
+
+console.log('Server Listening on port 8080!');
