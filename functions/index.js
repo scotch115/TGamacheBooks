1bb9a5ee09ed86c8cde1152a110b179fd10fe9f8
scotch115 <jordangamache115@gmail.com>
2019-11-13 13:48:22 -0500

    Successfully integrated Firebase Database to site. Currently using realtime database rather than firestore. Will further look into possibly switching later on.

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,6 @@ const app = express();
 // app.use(express.static('public'));
 
 // app.use(express.static(__dirname + "/../public"));
-var firebase = require('firebase/app');
 
 app.get('/', function(req, res)  {
   res.sendFile(__dirname + '/public/index.html');
@@ -24,20 +23,6 @@ app.get('/blog', function(req, res) {
 	res.sendFile(__dirname + '/public/test.html');
 });
 
-// Initialize Firebase
-	var config = {
-		apiKey: "AIzaSyAuqXCgR7z4DgDmTOXWOyTJq_o0IO9NsLY",
-		authDomain: "tomgamachebooks.firebaseapp.com",
-		databaseURL: "https://tomgamachebooks.firebaseio.com",
-		projectId: "tomgamachebooks",
-		storageBucket: "tomgamachebooks.appspot.com",
-		messagingSenderId: "233346595074"
-	};
-	firebase.initializeApp(config);
-
-	var database = firebase.database();
-
-	return firebase.database().ref('/articleBody/')
 
 exports.app = functions.https.onRequest(app);
 // module.exports = {
