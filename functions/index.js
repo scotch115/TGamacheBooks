4bf12759430ee19686dafd638ebe2a49b9a460e8
scotch115 <jordangamache115@gmail.com>
2019-11-13 19:59:35 -0500

    Fixed all routing issues, cleaned up source code, everythign is up to date. Website is (temporarily) done

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/posts'));
+
 app.get('/', function(req, res)  {
   res.sendFile(__dirname + '/public/index.html');
 });
