d20d68d9351d0f2ff964a5fb1ba725a16fb4db54
scotch115 <jordangamache115@gmail.com>
2020-04-05 16:48:39 -0400

    Fixing merge issues after React port

---
+++ build/precache-manifest.2a05a596a8fb54d74ed88910c1c8c47f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f345850b937f115769eec2217517f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a95782cb445284edd4",
+    "url": "/static/css/main.bc09dea3.chunk.css"
+  },
+  {
+    "revision": "5313fc24697fe1e991a6",
+    "url": "/static/js/2.b372e1a7.chunk.js"
+  },
+  {
+    "revision": "e080c83ccbc1bb3b3e79f648d5115474",
+    "url": "/static/js/2.b372e1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a95782cb445284edd4",
+    "url": "/static/js/main.4bcd4203.chunk.js"
+  },
+  {
+    "revision": "44e82df5afaa7171cec6",
+    "url": "/static/js/runtime-main.0c955a52.js"
+  },
+  {
+    "revision": "c627d4dac47903c53fbf83dfa477db1e",
+    "url": "/static/media/about.c627d4da.png"
+  },
+  {
+    "revision": "b9718581ca84d2e8ef4f8eb39e52e34e",
+    "url": "/static/media/notsocommoncover.b9718581.png"
+  },
+  {
+    "revision": "85bdb231ae57d39db9cb5832f3d58138",
+    "url": "/static/media/profile.85bdb231.png"
+  }
+]);
